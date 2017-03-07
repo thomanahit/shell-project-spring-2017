9bc109876908d51125a78663ac03ee09aed4eac9
Michael Zakaryan <mikazakaryan94@gmail.com>
2017-03-07 16:10:04 +0400

    add a checking of '<' '>' '|'

--- shell.cpp
+++ shell.cpp
@@ -1,10 +1,19 @@
 #include "shell.h"
 
-std::vector <std::string> preasing (std::string input_line){
+std::vector <std::string> parsing (std::string input_line){
+  for (int i = 0; i < input_line.size(); i++) {
+    if(input_line.at(i) == '<'
+    || input_line.at(i) == '>'
+    || input_line.at(i) == '|'){
+      input_line.insert(i, " ");
+      input_line.insert(i + 2, " ");
+      i++;
+    }
+  }
   std::stringstream ss (input_line) ;
   std::vector<std::string> return_value ;
   for ( std::string word; ss>>word;) {
-  return_value.push_back(word);
+    return_value.push_back(word);
   }
   for (int i = 0; i < return_value.size(); i++) {
     std::cout << return_value[i] << std::endl;
@@ -16,5 +25,5 @@ void get_comand(void) {
   std::string input_line;
   std::vector <std::string> praserd_commands;
   getline (std::cin, input_line) ;
-  praserd_commands = preasing(input_line);
+  praserd_commands = parsing(input_line);
 }
